1c1734b55c42a9341436256f51500d4b36b21e01
5t31thphoto <5t31thphoto@gmail.com>
2026-09-14 16:46:14 -0600

    Add files via upload

---
+++ firmware/main/generated/config.h
@@ -0,0 +1,6 @@
+#pragma once
+/* This file is overwritten by CI from the web-flasher config.
+ * Defaults for local builds: */
+#define FOX_NAME "Ember"
+#define FOX_MODE "groq"
+#define FOX_COLOR_PRIMARY "#ff6b35"
